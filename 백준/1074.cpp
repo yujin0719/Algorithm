69c9758a3ed2145663baa3a5b4c73ee58c7f0421
yujin <yujin1760@naver.com>
2020-04-22 16:15:02 +0900

    recursion

---
+++ 백준/1074.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,r,c,cnt =0;
+
+void Z(int size, int row, int column){
+  if(size == 2){
+    if(row == r && column == c){
+      cout << cnt;
+      return;
+    }
+    cnt++;
+    if(row == r && column+1 == c){
+      cout << cnt;
+      return;
+    }
+    cnt++;
+    if(row+1 == r && column == c){
+      cout << cnt;
+      return;
+    }
+    cnt++;
+    if(row+1 == r && column+1 == c){
+      cout << cnt;
+      return;
+    }
+    cnt++;
+    return;
+  }
+  Z(size/2,row,column);
+  Z(size/2,row,column+size/2);
+  Z(size/2,row+size/2,column);
+  Z(size/2,row+size/2,column+size/2);
+}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> r >> c;
+  Z(1 << N, 0, 0);
+}
---
+++ 백준/11729.cpp
@@ -0,0 +1,9 @@
+//
+//  11729.cpp
+//  baekjoon
+//
+//  Created by 최유진 on 2020/04/18.
+//  Copyright © 2020 최유진. All rights reserved.
+//
+
+#include <stdio.h>

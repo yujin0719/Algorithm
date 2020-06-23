09afcd8ac04b27c6e091fba7f7cdd15dc7dbd89f
yujin <yujin1760@naver.com>
2020-06-23 15:39:10 +0900

    lv1.같은 숫자는 싫어

---
+++ 프로그래머스/lv1.같은 숫자는 싫어.cpp
@@ -0,0 +1,17 @@
+// Level1 같은 숫자는 싫어
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    answer.push_back(arr[0]);
+    for(int i = 1; i < arr.size(); i++){
+        if(arr[i] != arr[i-1])
+            answer.push_back(arr[i]);
+    }
+
+    return answer;
+}

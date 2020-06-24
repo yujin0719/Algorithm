75e179461011d08455fb75dd850d1f65747df58d
yujin <yujin1760@naver.com>
2020-06-24 14:46:06 +0900

    lv1.소수 찾기

--- 프로그래머스/lv1.소수찾기.cpp
+++ 프로그래머스/lv1.소수찾기.cpp
@@ -1,4 +1,4 @@
-// level1 소수 찾기
+//level1 소수찾기 
 #include <string>
 #include <vector>
 
@@ -6,16 +6,12 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
+    vector<bool> prime(n,true);
     for(int i = 2; i <= n; i++){
-        bool flag = true;;
-        for(int j = 2; j*j <= i; j++){
-            if(i%j == 0){
-                flag = false;
-                break;
-            }
-        }
-        if(flag == true)
-            answer += 1;
+        if(prime[i] == false) continue;
+        for(int j = i*2; j <= n; j+=i) prime[j] = false;
     }
+    for(int i = 2; i <= n; i++)
+        if(prime[i] == true) answer += 1;
     return answer;
 }

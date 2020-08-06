5269b9bf3a69576293ef9b987a44c08be39ae43d
yujin <yujin1760@naver.com>
2020-08-06 12:58:21 +0900

    1449 : 수리공항승: q!
    n,L = map(int,input().split())
    data = list(map(int,input().split()))
    data.sort()
    
    fix = [0] * 1001
    cnt = 0
    
    for i in data:
      if fix[i] == 0:
        cnt += 1
        for j in range(i,i+L):
          if j < 1001:
            fix[j] += 1
    
    print

---
+++ 백준.py/1449.py
@@ -0,0 +1,17 @@
+// 1449 : 수리공항승
+
+n,L = map(int,input().split())
+data = list(map(int,input().split()))
+data.sort()
+
+fix = [0] * 1001
+cnt = 0
+
+for i in data:
+  if fix[i] == 0:
+    cnt += 1
+    for j in range(i,i+L):
+      if j < 1001:
+        fix[j] += 1
+
+print(cnt)

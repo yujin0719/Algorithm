155364e0fcc633f5458742554685ab957328ce6b
yujin <yujin1760@naver.com>
2020-09-22 14:31:39 +0900

    18352: 특정 거리의 도시 찾기

---
+++ 백준.py/18352.py
@@ -0,0 +1,33 @@
+# 18352: 특정거리의 도시찾기
+# 이문제 입력받을때 map(int,input().split())히면 시간초과나온다..
+
+from collections import deque
+import sys
+
+# n 도시의 개수 / m 도로의 개수 / k 거리정보 / x 출발 도시의 번호
+n,m,k,x = map(int,sys.stdin.readline().rstrip().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    temp_x, temp_y = map(int,sys.stdin.readline().rstrip().split())
+    graph[temp_x].append(temp_y)
+
+distance = [-1] * (n + 1)
+distance[x] = 0
+
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next in graph[now]:
+        if distance[next] == -1:
+            distance[next] = distance[now] + 1
+            q.append(next)
+
+check = False
+for i in range(1,n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)

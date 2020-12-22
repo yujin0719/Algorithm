8a8fa3e05958faaf0a55fb20ab62b2197badeffb
yujin <yujin1760@naver.com>
2020-12-22 16:22:35 +0900

    H-index

---
+++ 프로그래머스/H-index.cpp
@@ -0,0 +1,15 @@
+// H-index (정렬)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(),citations.end(),greater<>());
+    if (!citations[0]) return 0;
+    for (int i = 0; i < citations.size(); i++)    {
+        if(citations[i] > i) answer++;
+        else break;
+    }
+    return answer;
+}

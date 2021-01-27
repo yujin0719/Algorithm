3083732a0a805e47d8bad03080b490971df6b201
repl.it user <replituser@example.com>
2021-01-27 15:30:28 +0000

    modify

--- 프로그래머스/lv2.점프와 순간 이동.py
+++
@@ -1,9 +0,0 @@
-# Level2: 점프와 순간 이동
-
-def solution(n):
-    answer = 1
-    while n > 1:
-        if n % 2 != 0:
-            answer += 1
-        n = n // 2
-    return answer
--- 프로그래머스/가운데 글자 가져오기(좋은풀이).cpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-using namespace std;
-
-string solution(string s) {
-    return s.length()&1 ? s.substr(s.length()*0.5,1) : s.substr(s.length()*0.5-1,2);
-}

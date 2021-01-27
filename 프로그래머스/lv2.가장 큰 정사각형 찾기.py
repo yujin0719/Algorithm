07f31faed221bb926aa16e0c06e888c2befd87b2
yujin0719 <40332307+yujin0719@users.noreply.github.com>
2021-01-28 00:36:19 +0900

    Delete 프로그래머스 directory

--- 프로그래머스/lv2.가장 큰 정사각형 찾기.py
+++
@@ -1,8 +0,0 @@
-# Level2: 가장 큰 정사각형 찾기
-
-def solution(board):
-    for i in range(1,len(board)):
-        for j in range(1,len(board[i])):
-            if board[i][j] > 0:
-                board[i][j] = min(board[i-1][j-1],board[i-1][j],board[i][j-1]) + 1
-    return max(max(b) for b in board)**2

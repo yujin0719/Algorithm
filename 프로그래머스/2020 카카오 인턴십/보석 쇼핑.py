9d79d57a81ff18f28dfbdd0447ac33384f95a57d
yujin <yujin1760@naver.com>
2021-04-09 14:29:39 +0900

    보석 쇼핑

---
+++ 프로그래머스/2020 카카오 인턴십/보석 쇼핑.py
@@ -0,0 +1,24 @@
+# 보석 쇼핑 (효율성)
+from collections import defaultdict
+def solution(gems):
+    answer = []
+    jewelry = len(set(gems))
+    result = 100000 
+    dict = defaultdict(int)
+    left,right = 0,0
+    dict[gems[right]] += 1
+    while right < len(gems) and left < len(gems):
+        if len(dict) == jewelry:
+            if result > right-left:
+                result = right-left
+                answer = [left+1,right+1]
+            dict[gems[left]] -= 1
+            if dict[gems[left]] == 0:
+                del dict[gems[left]]
+            left += 1
+        else :
+            right += 1
+            if right == len(gems):
+                break
+            dict[gems[right]] += 1
+    return answer

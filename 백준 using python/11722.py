652baa3c1ccfca153c1f78432d42bab002ba3a26
yujin <yujin1760@naver.com>
2021-04-10 15:07:35 +0900

    11722: 가장 긴 감소하는 부분 수열

---
+++ 백준 using python/11722.py
@@ -0,0 +1,9 @@
+# 11722: 가장 긴 감소하는 부분 수열
+n = int(input())
+number = list(map(int,input().split(" ")))
+dp = [1 for _ in range(n)]
+for i in range(1,n):
+	for j in range(i):
+		if number[i] < number[j]:
+			dp[i] = max(dp[i],dp[j]+1)
+print(max(dp))

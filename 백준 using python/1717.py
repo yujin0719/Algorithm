559d3695bcdbc521d62af129d9e0c61ede8ff8db
yujin <yujin1760@naver.com>
2021-04-14 18:54:39 +0900

    1717: 집합의 표현

--- 백준 using python/1717.py
+++ 백준 using python/1717.py
@@ -1,1 +1,29 @@
 # 1717: 집합의 표현
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+def find(target):
+    if parent[target] != target:
+        parent[target] = find(parent[target])
+    return parent[target]
+
+def union(a,b):
+	a = find(a)
+	b = find(b)
+	if a < b:
+		parent[b] = a
+	else :
+		parent[a] = b
+
+n,m = map(int,input().split(" "))
+parent = [x for x in range(n+1)]
+for _ in range(m):
+	command,a,b = map(int,input().split(" "))
+	if command :
+		if find(a) == find(b):
+			print("YES")
+		else :
+			print("NO")
+	else:
+		union(a,b)

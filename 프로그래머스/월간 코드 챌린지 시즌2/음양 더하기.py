7d076b9d9ec622676fcd99556ffec42c2d15f4ef
yujin <yujin1760@naver.com>
2021-04-20 16:08:00 +0900

    음양 더하기

---
+++ 프로그래머스/월간 코드 챌린지 시즌2/음양 더하기.py
@@ -0,0 +1,9 @@
+# 음양 더하기
+def solution(absolutes, signs):
+    answer = 0
+    for a,s in zip(absolutes,signs):
+        if not s:
+            answer -= a
+        else :
+            answer += a
+    return answer

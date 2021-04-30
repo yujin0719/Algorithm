cdf03e473caab303bcfa6319ea867460d7b241eb
yujin <yujin1760@naver.com>
2021-04-30 20:10:31 +0900

    Level2: H-index using python

---
+++ 프로그래머스/lv2.H-index.py
@@ -0,0 +1,8 @@
+# Level2: H-index
+def solution(citations):
+    citations.sort()
+    n = len(citations)
+    for i in range(n):
+        if citations[i] >= n-i:
+            return n-i
+    return 0

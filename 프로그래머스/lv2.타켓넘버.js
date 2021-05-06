4658303bf286e9d9c83810a942d7619773709428
yujin <yujin1760@naver.com>
2021-05-06 16:41:54 +0900

    Level2: 타켓넘버 using Javascript

--- 프로그래머스/lv2.타켓넘버.js
+++ 프로그래머스/lv2.타켓넘버.js
@@ -1,1 +1,15 @@
 // Level2: 타켓넘버
+function solution(numbers, target) {
+    let answer = 0;
+    function dfs(idx,total,numbers,target){
+        if(idx === numbers.length){
+            if(target === total) answer += 1;
+        }
+        else{
+            dfs(idx+1,total+numbers[idx],numbers,target);
+            dfs(idx+1,total-numbers[idx],numbers,target);
+        }
+    }
+    dfs(0,0,numbers,target)
+    return answer;
+}

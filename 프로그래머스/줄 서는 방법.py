6dbc9f75ef3dc84bcc84b8fc94f83c10531274ef
yujin <yujin1760@naver.com>
2021-06-17 11:24:21 +0900

    줄 서는 방법

---
+++ 프로그래머스/줄 서는 방법.py
@@ -0,0 +1,15 @@
+# 줄 서는 방법
+import math
+def solution(n, k):
+    answer = []
+    number = [i+1 for i in range(n)]
+    total, stage = 1, n-1
+    while number:
+        for i in range(1,len(number)+1):
+            if k < total + math.factorial(stage) * i:
+                answer.append(number[i-1])
+                total += (math.factorial(stage) * (i-1))
+                del number[i-1]
+                stage -= 1
+                break
+    return answer

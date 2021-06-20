a9a5da706db910b8585210b89843f5d25712be8d
yujin <yujin1760@naver.com>
2021-06-20 13:42:28 +0900

    Level2: 조이스틱

---
+++ 프로그래머스/lv2.조이스틱.py
@@ -0,0 +1,21 @@
+# Level2: 조이스틱
+def solution(name):
+    answer = 0
+    position = 0
+    vertical = [min(ord(n) - ord('A'), ord('Z') - ord(n) + 1) for n in name]
+    while True:
+        answer += vertical[position]
+        vertical[position] = 0 
+        if sum(vertical) == 0:
+            return answer
+        left,right = 0,0
+        while vertical[position - left] == 0:
+            left += 1
+        while vertical[right + position] == 0:
+            right += 1
+        if left >= right :
+            answer += right
+            position = right
+        else :
+            answer += left
+            position = -left

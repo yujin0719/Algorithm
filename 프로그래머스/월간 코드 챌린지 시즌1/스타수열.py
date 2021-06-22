19849181d9b56887629f4ab07af5e1fbbc7691a8
yujin <yujin1760@naver.com>
2021-06-22 21:28:05 +0900

    스타 수열

---
+++ 프로그래머스/월간 코드 챌린지 시즌1/스타수열.py
@@ -0,0 +1,18 @@
+# 스타수열
+from collections import Counter
+def solution(a):
+    answer = 0
+    cnt = Counter(a)
+    for element in cnt:
+        if cnt[element] <= answer:
+            continue
+        total = 0
+        idx = 0
+        while idx < len(a) - 1:
+            if((a[idx] != element and a[idx+1] != element) or a[idx] == a[idx+1]):
+                idx += 1
+                continue
+            total += 1
+            idx += 2
+        answer = max(total,answer)
+    return answer*2

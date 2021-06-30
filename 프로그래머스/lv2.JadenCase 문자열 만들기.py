38fe54e88effefc1cf473fcd4559413a62ae99d9
yujin <yujin1760@naver.com>
2021-06-30 19:10:23 +0900

    Level2:JadenCase 문자열 만들기

--- 프로그래머스/lv2.JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2.JadenCase 문자열 만들기.py
@@ -1,10 +1,17 @@
 # Level2: JadenCase 문자열 만들기
+# def solution(s):
+#     answer = ''
+#     answer += s[0].upper()
+#     for i in range(1,len(s)):
+#         if s[i-1] == " ":
+#             answer += s[i].upper()
+#         else :
+#             answer += s[i].lower()
+#     return answer
+
 def solution(s):
+    s = s.lower().split(" ")
     answer = ''
-    answer += s[0].upper()
-    for i in range(1,len(s)):
-        if s[i-1] == " ":
-            answer += s[i].upper()
-        else :
-            answer += s[i].lower()
-    return answer
+    for word in s:
+        answer += (word.capitalize() + " ")
+    return answer[:-1]
